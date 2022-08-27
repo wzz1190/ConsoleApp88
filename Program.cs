21e3c0569d09c85cfcd738d182b9141e18274a04
Administrator <Administrator@PC-202107151145>
2022-08-27 13:20:58 +0800

    1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp88
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World！!");
         }
     }
 }
