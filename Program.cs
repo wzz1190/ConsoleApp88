386d19026e92a5b96cdbdcdeab2480625e4808de
Administrator <Administrator@PC-202107151145>
2022-08-27 13:15:56 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp88
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
